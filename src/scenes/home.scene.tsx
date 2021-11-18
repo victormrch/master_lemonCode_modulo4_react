4348c1caea79739c3eede555d102db67aaa4a6b9
“Victor <victor.raman@eu.jll.com>
2021-11-18 17:54:44 +0100

    Create World Cuisine Layout

--- src/scenes/home.scene.tsx
+++ src/scenes/home.scene.tsx
@@ -1,5 +1,6 @@
 import { routes } from 'core/router/routes';
 import { FooterLayout } from 'layouts/footer/footer.layouts';
+import { WorldCuisineLayout } from 'layouts/world-cuisine/world-cuisine.layouts';
 import { FeatureRecipeContainer } from 'pods/feature-recipe';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -9,6 +10,7 @@ export const HomeScene: React.FC = () => {
     <>
       <FeatureRecipeContainer />
       <Link to={routes.recipe('123')}>Navigate to recipe</Link>
+      <WorldCuisineLayout />
       <FooterLayout />
     </>
   );
